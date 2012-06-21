6807b739e2a92e9bfc2811ba5e4c7a5653956858
sergerehem <serge.rehem@gmail.com>
2012-06-21 11:26:40 -0300

    new user business controller

---
+++ src/main/java/business/UserBC.java
@@ -0,0 +1,23 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import entity.User;
+
+public class UserBC {
+	private static Map<String, User> userList = new HashMap<String, User>();
+	
+	public boolean login(String email, String name, Float latitude, Float longitude) {
+		
+		if (userList.get(email) != null)
+			userList.put(email, new User(email, name, latitude, longitude));
+		return true;
+	}
+
+	public ArrayList<User> list() {
+		return (ArrayList<User>) userList.values();
+	}
+
+}
