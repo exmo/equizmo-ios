808caba770daaba18e4c51db1ddd2d3c0c8725c3
sergerehem <serge.rehem@gmail.com>
2012-06-21 11:26:18 -0300

    new method for user list

--- src/main/java/webservice/UserWS.java
+++ src/main/java/webservice/UserWS.java
@@ -1,16 +1,27 @@
 package webservice;
 
+import java.util.ArrayList;
+
+import javax.inject.Inject;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.servlet.annotation.WebServlet;
+
+import business.UserBC;
+import entity.User;
 @WebService
 @WebServlet("/user")
 public class UserWS {
+	@Inject UserBC bc;
 	
 	@WebMethod
-	public boolean login(@WebParam(name="email") String email, @WebParam(name="name") String name){
-		return true;
+	public boolean login(@WebParam(name="email") String email, @WebParam(name="name") String name, @WebParam(name="latitude") Float latitude, @WebParam(name="longituge") Float longitude){
+		return bc.login(email, name, latitude, longitude);
 	}	
 
+	@WebMethod
+	public ArrayList<User> list(){
+		return bc.list();
+	}	
 }
