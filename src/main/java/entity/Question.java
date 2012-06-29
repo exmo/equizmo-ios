80e221fc7e42ff2f164c0c7c7943b1896ef5bbfb
sergerehem <serge.rehem@gmail.com>
2012-06-29 14:42:46 -0300

    Refactoring

--- src/main/java/entity/Question.java
+++ src/main/java/entity/Question.java
@@ -7,8 +7,20 @@ import java.util.List;
 public class Question {
 	private String enunciation;
 	//private List<String> propositions = new ArrayList<String>();
+	private String[] propositions; // = new ArrayList<String>();
 	private int answer;
 
+	public Question() {
+		super();
+	}
+	
+	public Question(String enunciation, String[] propositions, int answer) {
+		super();
+		this.enunciation = enunciation;
+		this.propositions = propositions;
+		this.answer = answer;
+	}
+	
 	public String getEnunciation() {
 		return enunciation;
 	}
@@ -25,13 +37,13 @@ public class Question {
 	// this.propositions = propositions;
 	// }
 
-//	public String[] getPropositions() {
-//		return (String[]) propositions.toArray();
-//	}
-//
-//	public void setPropositions(String[] propositions) {
-//		this.propositions = Arrays.asList(propositions);
-//	}
+	public String[] getPropositions() {
+		return propositions; //(String[]) propositions.toArray();
+	}
+
+	public void setPropositions(String[] propositions) {
+		this.propositions = propositions; //Arrays.asList(propositions);
+	}
 
 	public int getAnswer() {
 		return answer;
@@ -40,4 +52,5 @@ public class Question {
 	public void setAnswer(int answer) {
 		this.answer = answer;
 	}
+
 }
