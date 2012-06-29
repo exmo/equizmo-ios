4e91adc9ee182137325736db83c345cc73a3039d
sergerehem <serge.rehem@gmail.com>
2012-06-29 16:13:08 -0300

    Refactored

--- src/main/java/persistence/CategoryDAO.java
+++ src/main/java/persistence/CategoryDAO.java
@@ -3,20 +3,13 @@ package persistence;
 import java.util.ArrayList;
 import java.util.List;
 
+import util.FileUtil;
+
 public class CategoryDAO {
 	private static List<String> categoryList = new ArrayList<String>();
 
-	public boolean add(String category) {
-		if (categoryList.contains(category))
-			return false;
-		categoryList.add(category);
-		return true;
-	}
-
 	public List<String> list() {
-		
-		//"https://raw.github.com/exmo/exmo.github.com/master/equizmo/categories.txt"
-		return categoryList;
+		return FileUtil.readRemoteAsStringList("https://raw.github.com/exmo/exmo.github.com/master/equizmo/categories.txt");
 	}
 
 }
--- src/test/java/persistence/CategoryDAOTest.java
+++ src/test/java/persistence/CategoryDAOTest.java
@@ -1,7 +1,5 @@
 package persistence;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,25 +7,9 @@ import org.junit.Test;
 public class CategoryDAOTest {
 
 	@Test
-	public void addCategoryWithSuccessShouldReturnTrue() {
-		CategoryDAO dao = new CategoryDAO();
-		assertTrue(dao.add("Category 1"));
-	}
-
-	@Test
-	public void addExistingCategoryShouldReturnFalse() {
-		CategoryDAO dao = new CategoryDAO();
-		assertFalse(dao.add("Category 1"));
-	}
-
-	@Test
 	public void listCategories() {
 		CategoryDAO dao = new CategoryDAO();
-		dao.add("Category 1");
-		dao.add("Category 2");
-		assertEquals(dao.list().size(), 2);
-		assertEquals("Category 1", dao.list().get(0));
-		assertEquals("Category 2", dao.list().get(1));
+		assertTrue(dao.list().size() > 0);
 	}
 
 }
