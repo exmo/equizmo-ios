bfdd6900ea6fe1bbb94dcc4cc627cba09ae30ebb
sergerehem <serge.rehem@gmail.com>
2012-06-29 16:12:57 -0300

    test for RemoteFile Util

--- src/test/java/util/RemoteFileTest.java
+++ src/test/java/util/RemoteFileTest.java
@@ -1,5 +1,31 @@
 package util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
 public class RemoteFileTest {
 
+	@Test
+	public void readRemote() {
+		String url = "https://raw.github.com/exmo/equizmo-ws/master/src/test/resources/remote.txt";
+		String expectedContent = "this file should be read remotely from github site";
+		assertEquals(expectedContent, FileUtil.readRemote(url));
+	}
+	
+	@Test
+	public void readRemoteAsList() {
+		String url = "https://raw.github.com/exmo/equizmo-ws/master/src/test/resources/remote.txt";
+		List<String> sl = FileUtil.readRemoteAsStringList(url);
+		String expectedContent = "this file should be read remotely from github site";
+		assertEquals(expectedContent, sl.get(0));
+	}
+	
+	@Test
+	public void readInvalidRemote() {
+		assertNull(FileUtil.readRemote("http://www.nonexisting.url"));
+	}	
 }
