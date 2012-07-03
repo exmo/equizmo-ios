314512a425ea59c8b3af7ae7007dde969110639f
sergerehem <serge.rehem@gmail.com>
2012-07-03 11:12:12 -0300

    created startup method

--- src/main/java/persistence/CategoryDAO.java
+++ src/main/java/persistence/CategoryDAO.java
@@ -1,13 +1,25 @@
 package persistence;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import br.gov.frameworkdemoiselle.annotation.Startup;
+
+import entity.Quiz;
+
 import util.FileUtil;
+import util.XMLParserUtil;
 
 public class CategoryDAO {
-
+	List<String> categoryList;
+	
+	@Startup
+	public void loadCategories() {
+		categoryList = FileUtil.readRemoteAsStringList("https://raw.github.com/exmo/exmo.github.com/master/equizmo/categories.txt");
+	}
+	
 	public List<String> list() {
-		return FileUtil.readRemoteAsStringList("https://raw.github.com/exmo/exmo.github.com/master/equizmo/categories.txt");
+		return categoryList;
 	}
 
 }
