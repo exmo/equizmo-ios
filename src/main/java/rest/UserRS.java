769b488460b1d45c561d3869fde40ce63d92556b
Robson Ximenes <robsonximenes@gmail.com>
2012-07-04 13:02:51 -0300

    Conseranto o servico do rankin

--- src/main/java/rest/UserRS.java
+++ src/main/java/rest/UserRS.java
@@ -9,16 +9,7 @@ import business.UserBC;
 
 @Path("/user")
 public class UserRS {
-<<<<<<< HEAD
-    @Inject 
-    UserBC bc;
-    
-    @GET
-    @Path("/addPoints/{email}/{points}")
-    public int addPoints(@PathParam("email") String email, @PathParam("points") int points) {
-        return bc.addPoints(email, points);
-    }        
-=======
+
 	@Inject 
 	UserBC bc;
 	
@@ -27,5 +18,5 @@ public class UserRS {
 	public int addPoints(@PathParam("email") String email, @PathParam("points") int points) {
 		return bc.addPoints(email, points);
 	}		
->>>>>>> d0586c3c0143f27ddf36726eb485e7af53233a93
+	
 }
