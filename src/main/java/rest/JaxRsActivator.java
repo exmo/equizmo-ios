14a93f18c9825f3bdf1e5caf9718e8758ff5ee3f
sergerehem <serge.rehem@gmail.com>
2012-07-04 09:36:10 -0300

    removed @controller annotation

--- src/main/java/rest/JaxRsActivator.java
+++ src/main/java/rest/JaxRsActivator.java
@@ -3,6 +3,9 @@ package rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import br.gov.frameworkdemoiselle.stereotype.Controller;
+
+@Controller
 @ApplicationPath("/rest")
 public class JaxRsActivator extends Application {
    /* class body intentionally left blank */
