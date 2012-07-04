152d8afd8388460831627de300253ffd6a8cb345
sergerehem <serge.rehem@gmail.com>
2012-07-04 14:54:10 -0300

    call bc.clear() method

--- src/test/java/business/UserBCTest.java
+++ src/test/java/business/UserBCTest.java
@@ -25,6 +25,7 @@ public class UserBCTest {
 
     @Test
     public void testRanking() {
+    	bc.clear();
         bc.login("john@email.com","John Travolta",10f,-10f);
         bc.login("olivia@email.com","Olivia Newton John",10f,-10f);
         bc.login("cage@email.com","Nicolas Cage",10f,-10f);
@@ -34,10 +35,10 @@ public class UserBCTest {
         bc.addPoints("cage@email.com", 30);
         bc.addPoints("nicole@email.com", 10);
         List<User> ranking = bc.ranking(4);
+        assertEquals(4,ranking.size());
         assertEquals("Olivia Newton John",ranking.get(0).getName());
         assertEquals("Nicolas Cage",ranking.get(1).getName());
         assertEquals("John Travolta",ranking.get(2).getName());
         assertEquals("Nicole Kidman",ranking.get(3).getName());
-        assertEquals(4,ranking.size());
     }
 }
